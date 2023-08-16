9b9ba7e32640a01ad38b30ef4e75e63bee72e622
Usman Khan <ukusmankhanuk1@gmail.com>
2023-08-16 10:06:50 +0500

    Flone word of title changed to Himaliya

--- src/pages/home/HomeOrganicFood.js
+++ src/pages/home/HomeOrganicFood.js
@@ -15,6 +15,7 @@ const HomeOrganicFood = () => {
       <SEO
         titleTemplate="Pure Shilajit"
         description="Organic food home of flone react minimalist eCommerce template."
+        title= "Himaliya"
       />
       <LayoutThree
         headerTop="visible"
